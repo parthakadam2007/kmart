bdd0610e054de02e172594e6cc6fd6cd2d95b31d
Partha Kadam <parthakadam2007@gmail.com>
2025-10-30 18:53:07 +0530

    first commit

---
+++ src/main/java/com/example/kmart/KmartApplication.java
@@ -0,0 +1,13 @@
+package com.example.kmart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KmartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KmartApplication.class, args);
+	}
+
+}
